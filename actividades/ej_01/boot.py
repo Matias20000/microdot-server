549948c6eec4af61c855ee7b611ca5a876689b68
Matias20000 <matias.rapero2007@gmail.com>
2025-08-24 16:44:20 -0300

    ej_01

--- actividades/ej_01/boot.py
+++ actividades/ej_01/boot.py
@@ -1,1 +1,46 @@
-# Configuracion inicial
+import network
+
+ssid = 'Cooperadora Alumnos'
+password = ' '
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(ssid, password)
+
+while not wlan.isconnected():
+    pass
+
+print('Conectado a WiFi:', wlan.ifconfig())
+
+from microdot import Microdot
+
+app = Microdot()
+
+@app.route('/')
+def index(request):
+    return '''
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Servidor</title>
+  </head>
+  <body>
+    <main>
+      <header>
+        <h1>
+          <span style="font-size:2.5em; font-weight:bold; letter-spacing:2px;">Aca mi server de MicroDot </span>
+        </h1>
+      </header>
+      <section>
+        <p>
+          <span style="font-size:1.3em; color:#2c3e50;">Este servidor funciona correctamente.</span>
+        </p>
+      </section>
+    </main>
+  </body>
+</html>
+    ''', 200, {'Content-Type': 'text/html'}
+
+app.run(port=80)
