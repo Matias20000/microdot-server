b39072f69cca0a9226ecef479bca7147e540b34b
Matias20000 <101536985+Matias20000@users.noreply.github.com>
2025-12-19 14:34:31 -0300

    Base.js

--- actividades/ej_03/styles/scripts/base.js
+++ actividades/ej_03/styles/scripts/base.js
@@ -1,6 +1,47 @@
 document.addEventListener('DOMContentLoaded', () => {
     const fechaElemento = document.getElementById('fecha');
     const fechaActual = new Date(document.lastModified);
-    const opcionesFecha = { year: 'numeric', month: 'long', day: 'numeric' };
-    fechaElemento.textContent = fechaActual.toLocaleDateString('es-ES', opcionesFecha);
+    fechaElemento.textContent = fechaActual.toLocaleDateString('es-ES', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    });
+
+    updateSetpointValue(
+        document.getElementById("setpoint-slider").value
+    );
 });
+
+async function readTemperature() {
+    const data = await fetchData('/sensors/ds18b20/read');
+
+    const tempEl = document.getElementById("mostrar-temperatura");
+
+    if (data) {
+        tempEl.innerText = data.temperatura;
+        tempEl.parentElement.classList.add("pulse");
+    } else {
+        tempEl.innerText = "Error";
+    }
+}
+
+async function sendSetpoint() {
+    const value = Number(document.getElementById("setpoint-slider").value);
+    const data = await fetchData(`/setpoint/set/${value}`);
+
+    const buzzerEl = document.getElementById("estado-buzzer");
+
+    if (data) {
+        buzzerEl.innerText = data.buzzer;
+    } else {
+        buzzerEl.innerText = "Error";
+    }
+}
+
+function updateSetpointValue(value) {
+    document.getElementById("setpoint-value").innerText = value;
+    sendSetpoint();
+}
+
+setInterval(readTemperature, 1000);
+readTemperature();
